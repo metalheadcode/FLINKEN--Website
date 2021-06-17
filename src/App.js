2dccbcb5406e8fbc54dbf5f749877bd9b2d9d8cd
Ahmad Hazim <metalheadcoder@gmail.com>
2021-06-17 17:29:33 +0800

    add route for About Us and contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Container from "reactstrap/lib/Container";
 import FootersLayoutsTypeEighth from "./layouts/FootersLayouts/FootersLayoutsTypeEighth/FootersLayoutsTypeEighth";
 import Header from "./layouts/HeroLayouts/Header/Header";
+import AboutUs from "./screens/AboutUs";
+import Contacts from "./screens/Contacts";
 import HomeScreen from "./screens/HomeScreen";
 import ProductDetail from "./screens/ProductDetail";
 
@@ -14,6 +16,8 @@ const App = () => {
             </Container>
             <Switch>
                 <Route exact path="/" render={(props) => <HomeScreen {...props} />} />
+                <Route exact path="/about-us" render={(props) => <AboutUs {...props} />} />
+                <Route exact path="/contacts" render={(props) => <Contacts {...props} />} />
                 <Route path="/products/:id" render={(props) => <ProductDetail {...props} />} />
             </Switch>
             <FootersLayoutsTypeEighth />
