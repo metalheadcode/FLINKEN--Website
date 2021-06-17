8353a700f69165d7a8bfa80967788c12d064b621
Ahmad Hazim <metalheadcoder@gmail.com>
2021-06-17 17:26:45 +0800

    create contact us screen

---
+++ src/screens/Contacts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FormsLayoutsTypeFourth from "../layouts/FormsLayouts/FormsLayoutsTypeFourth/FormsLayoutsTypeFourth";
+
+const Contacts = () => {
+    return (
+        <>
+            <FormsLayoutsTypeFourth />
+        </>
+    );
+};
+
+export default Contacts;
