58e7de56dabd642901b510c8dee1bed87df661ff
Ahmad Hazim <metalheadcoder@gmail.com>
2021-06-21 17:04:40 +0800

    update home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import EcommerceLayoutsTypeFourteenth from "../layouts/Ecommerce/EcommerceLayoutsTypeFourteenth/EcommerceLayoutsTypeFourteenth";
 import FormsLayoutsTypeFirst from "../layouts/FormsLayouts/FormsLayoutsTypeFirst/FormsLayoutsTypeFirst";
 import TestimonialsLayoutsTypeFifth from "../layouts/TestimonialsLayouts/TestimonialsLayoutsTypeFifth/TestimonialsLayoutsTypeFifth";
 import products from "../data/products";
 import HeroLayoutsTypeFifth from "../layouts/HeroLayouts/HeroLayoutsTypeFifth";
 import PortfolioLayoutsTypeSixth from "../layouts/PortfolioLayouts/PortfolioLayoutsTypeSixth/PortfolioLayoutsTypeSixth";
+import FlinkenContext from "../context/flinkenContext";
 
 const HomeScreen = (props) => {
+    const flinkenContext = useContext(FlinkenContext);
+    const { dark } = flinkenContext;
+
     return (
         <>
-            <HeroLayoutsTypeFifth />
-            <EcommerceLayoutsTypeFourteenth products={products} />
-            <PortfolioLayoutsTypeSixth />
-            <TestimonialsLayoutsTypeFifth />
-            <FormsLayoutsTypeFirst />
+            <HeroLayoutsTypeFifth dark={dark} />
+            <EcommerceLayoutsTypeFourteenth products={products} dark={dark} />
+            <PortfolioLayoutsTypeSixth dark={dark} />
+            <TestimonialsLayoutsTypeFifth dark={dark} />
+            <FormsLayoutsTypeFirst dark={dark} />
         </>
     );
 };
