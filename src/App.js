b9686f3f3443519c28547d3df118738d86460ee1
Ahmad Hazim <metalheadcoder@gmail.com>
2021-06-21 17:05:24 +0800

    update app js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Container from "reactstrap/lib/Container";
 import FootersLayoutsTypeEighth from "./layouts/FootersLayouts/FootersLayoutsTypeEighth/FootersLayoutsTypeEighth";
-import Header from "./layouts/HeroLayouts/Header/Header";
 import AboutUs from "./screens/AboutUs";
 import Contacts from "./screens/Contacts";
 import HomeScreen from "./screens/HomeScreen";
 import ProductDetail from "./screens/ProductDetail";
+import FlinkenState from "./context/FlinkenState";
+import Header from "./components/Header/Header";
 
 const App = () => {
     return (
-        <Router>
-            <Container>
-                <Header />
-            </Container>
-            <Switch>
-                <Route exact path="/" render={(props) => <HomeScreen {...props} />} />
-                <Route exact path="/about-us" render={(props) => <AboutUs {...props} />} />
-                <Route exact path="/contacts" render={(props) => <Contacts {...props} />} />
-                <Route path="/products/:id" render={(props) => <ProductDetail {...props} />} />
-            </Switch>
-            <FootersLayoutsTypeEighth />
-        </Router>
+        <FlinkenState>
+            <Router>
+                <Container>
+                    <Header />
+                </Container>
+                <Switch>
+                    <Route exact path="/" render={(props) => <HomeScreen {...props} />} />
+                    <Route exact path="/about-us" render={(props) => <AboutUs {...props} />} />
+                    <Route exact path="/contacts" render={(props) => <Contacts {...props} />} />
+                    <Route path="/products/:id" render={(props) => <ProductDetail {...props} />} />
+                </Switch>
+                <FootersLayoutsTypeEighth />
+            </Router>
+        </FlinkenState>
     );
 };
 
